4048df901a1916e5ec384c572a993cf16d3ca00b
hi_go <me@clutte.red>
2020-11-28 19:05:58 +0900

    Add `Front22.java`.

---
+++ Java/WarmUp1/Front22.java
@@ -0,0 +1,8 @@
+public String front22(String str) {
+    if(str.length()<2){
+        return str + str + str;
+    }else{
+        String first2 = str.substring(0,2);
+        return first2 + str + first2;
+    }
+}
